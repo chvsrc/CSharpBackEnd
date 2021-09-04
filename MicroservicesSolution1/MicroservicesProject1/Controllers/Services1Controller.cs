21c850ca23b15d80e1e4e57e5948962543296e7d
VENKAT <venkat@VENKATs-MacBook-Pro.local>
2021-09-03 20:18:39 -0400

    Added project

---
+++ MicroservicesSolution1/MicroservicesProject1/Controllers/Services1Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesProject1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Services1Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Services1-Controller-Data";
+        }
+    }
+}
---
+++ MicroservicesSolution1/MicroservicesProject2/Controllers/Services2Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesProject2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Services2Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Services2-Controller-Data";
+        }
+    }
+}
