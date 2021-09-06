f0aa96b69602ffadc4804f826a076b67d620cd48
VENKAT <venkat@VENKATs-MacBook-Pro.local>
2021-09-06 02:04:19 -0400

    Added gate way services

---
+++ MicroservicesSolution1/GatewayForMicroservices/Controllers/GatewayTempController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GatewayForMicroservices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayTempServicesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Gateway-Temp-Services-Data";
+        }
+    }
+}
